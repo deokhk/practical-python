3ce2048d4dbf1974d4bbd42be9c0ee8d15d0ed58
deokhk <deokhk@postech.ac.kr>
2021-09-29 01:57:33 +0900

    Day 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+num_bounce = 0
+meter = 100
+
+while num_bounce <10:
+    num_bounce = num_bounce + 1
+    meter = meter * 0.6
+    print(num_bounce, round(meter, ndigits=4))
